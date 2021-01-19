ae6959f182fcc487013ffb855caf43bc1d8ba0b9
CarterWilde <carterjwilde@gmail.com>
2021-01-18 21:11:38 -0700

    Additional Creation Info

--- src/Main.ts
+++ src/Main.ts
@@ -1,36 +1,38 @@
 import { Router } from "express";
-import {IRoute} from "./IRoute";
-import {IRouter} from "./IRouter";
+import { IRoute } from "./IRoute";
+import { IRouter } from "./IRouter";
 
-const prop = (obj:any, key:any) => {
+const prop = (obj: any, key: any) => {
     return obj[key].bind(obj);
 }
 
-const buildRoutes = (router: Router, ...routes: Array<IRoute>) : void => {
+const buildRoutes = (router: Router, ...routes: Array<IRoute>): void => {
     routes.forEach(endpoint => {
-            prop(router, endpoint.method.toLowerCase())(endpoint.url || '/', endpoint.handlers);
-            if(endpoint.routes) {
-                endpoint.routes.forEach(route => {
-                    route.url = (endpoint.url || '/') + route.url;
-                })
-                buildRoutes(router, ...endpoint.routes);
-            }
+        let url: string = endpoint.url || '/';
+        console.log("Route created:" + url);
+        prop(router, endpoint.method.toLowerCase())(url, endpoint.handlers);
+        if (endpoint.routes) {
+            endpoint.routes.forEach(route => {
+                route.url = url + route.url;
+            })
+            buildRoutes(router, ...endpoint.routes);
+        }
     });
 }
 
-const buildRouting = (...routers: Array<IRouter>) : Router => {
+const buildRouting = (...routers: Array<IRouter>): Router => {
     let router: Router = Router();
 
     routers.forEach(route => {
         let subRouter = Router();
-        
+
         route.middleware?.forEach(middle => {
             subRouter.use(middle.handler);
         });
 
         buildRoutes(subRouter, ...route.routes);
 
-        if(route.routers) {
+        if (route.routers) {
             subRouter.use(buildRouting(...route.routers));
         }
 
